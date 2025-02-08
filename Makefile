701a24e7fa74ae0c9359be6d96123584978bf324
Zjyhub <2313196292@qq.com>
2025-02-08 16:51:17 +0800

    feat(frontend):complete frontend service

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: gen-demo-proto
 gen-demo-proto:
 	@cd demo/demo_proto && cwgo server -I ../../idl --type RPC --module gomall/demo/demo_proto --service demo_proto --idl ../../idl/echo.proto 
+
+.PHONY: gen-frontend:
+gen-frontend:
+	cwgo server --type HTTP --idl ../../idl/frontend/home.proto --service frontend --module gomall/app/frontend -I ../../idl/
---
+++ app/frontend/biz/service/home.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	home "gomall/app/frontend/hertz_gen/frontend/home"
+
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+type HomeService struct {
+	RequestContext *app.RequestContext
+	Context        context.Context
+}
+
+func NewHomeService(Context context.Context, RequestContext *app.RequestContext) *HomeService {
+	return &HomeService{RequestContext: RequestContext, Context: Context}
+}
+
+func (h *HomeService) Run(req *home.Empty) (map[string]any, error) {
+	//defer func() {
+	// hlog.CtxInfof(h.Context, "req = %+v", req)
+	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
+	//}()
+	// todo edit your code
+	var resp = make(map[string]any)
+	items := []map[string]any{
+		{"Name": "T-shirt-1", "Price": 100, "Picture": "/static/image/t-shirt-1.jpeg"},
+		{"Name": "T-shirt-2", "Price": 200, "Picture": "/static/image/t-shirt-2.jpeg"},
+	}
+	resp["Items"] = items
+	resp["Title"] = "CloudWeGo Mall"
+	return resp, nil
+}
